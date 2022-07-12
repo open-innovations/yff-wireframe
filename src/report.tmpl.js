98e9f0d73d6e41310751c025a432ac7ebcb9607f
Giles Dring <giles@dringtech.com>
2022-07-12 15:40:48 +0100

    WIP on documenting functions

--- src/report.tmpl.js
+++ src/report.tmpl.js
@@ -1,6 +1,14 @@
 export const layout = 'report.njk';
 export const tags = ['report'];
 
+/**
+ * Generator function which creates a page per report data object.
+ * 
+ * The report object appears as a object of report definitions defined with a key.
+ * 
+ * @param { reports:  } options
+ */
+
 export default function* ({ reports }) {
   for (const report of Object.values(reports)) {
     yield {
