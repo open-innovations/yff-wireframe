6f825d437632c912282cda909d5872ccae58ec68
Giles Dring <giles@dringtech.com>
2022-07-19 12:34:47 +0100

    Switch to netlify cms lume plugin

--- _config.js
+++ _config.js
@@ -4,6 +4,8 @@ import basePath from "lume/plugins/base_path.ts";
 import postcss from "lume/plugins/postcss.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
+import netlifyCMS from "lume/plugins/netlify_cms.ts";
+
 
 const site = lume({
   src: './src',
@@ -17,8 +19,8 @@ site.use(basePath());
 site.use(resolveUrls());
 site.use(slugifyUrls());
 
-// Copy the admin directory
-site.copy("admin/config.yml");
+// Setup admin
+site.use(netlifyCMS());
 
 // Process all css files
 site.use(postcss({
